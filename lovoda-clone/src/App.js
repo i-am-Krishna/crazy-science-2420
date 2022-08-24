3583b19a3aceabe2d92579c5ed87402173bb0df1
Krishna Kumar Gupta <kumarkrishn663@gmail.com>
2022-08-24 21:29:58 +0530

    newPage part mapping have done

--- lovoda-clone/src/App.js
+++ lovoda-clone/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import MainRoutes from './Components/MainRoutes';
 
 function App() {
   return (
     <div className="App">
-    
+    <MainRoutes/>
     </div>
   );
 }
--- lovoda-clone/src/Pages/NewProducts.jsx
+++ lovoda-clone/src/Pages/NewProducts.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import SingleNewProduct from '../Components/SingleNewProduct'
+import { getNewPageData } from '../Redux/NewPage/action'
+import './newProduct.css'
 
 const NewProducts = () => {
-    
-  return (
-    <div>
+    const products = useSelector((store)=>store.newPage.newPageData)
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        dispatch(getNewPageData())
+    },[])
 
+  return (
+    <div className='new_product'>
+        {products.length > 0 && products.map((item)=>(
+        <SingleNewProduct key={item.id} {...item}/>
+        ))}
     </div>
   )
 }
