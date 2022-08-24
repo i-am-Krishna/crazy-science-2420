3a74e3f6d4f2190c1e6acde72a29434700a04691
Krishna Kumar Gupta <kumarkrishn663@gmail.com>
2022-08-24 18:12:12 +0530

    day2 new page data

--- lovoda-clone/src/Redux/App/action.js
+++ lovoda-clone/src/Redux/App/action.js
@@ -1,12 +1,1 @@
-import axios from 'axios';
-import * as types from './actionTypes';
-
-export const login =(params)=>(dispatch)=>{
-    dispatch({type:types.LOGIN_REQUEST})
-    return axios.post("https://masai-api-mocker.herokuapp.com/auth/login",params).then((res)=>dispatch({type:types.LOGIN_SUCCESS,payload:res.data.token})).catch((err)=>dispatch({type:types.LOGIN_FAILURE,payload:err}))
-}
-
-export const register =(params)=>(dispatch)=>{
-    dispatch({type:types.REGISTER_REQUEST})
-    return axios.post('https://masai-api-mocker.herokuapp.com/auth/register',params).then((res)=>dispatch({type:types.REGISTER_SUCCESS,payload:res.data})).catch((err)=>dispatch({type:types.REGISTER_FAILURE,payload:err}))
-}
+ 
