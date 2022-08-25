e7e0500648dad469e909f22a4228e7ebcdb466c6
swatisaulanki <100274438+swatisaulanki@users.noreply.github.com>
2022-08-25 22:27:37 +0530

    message

--- lovoda-clone/src/App.js
+++ lovoda-clone/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import CartPage from './Pages/CartPage';
+import PaymentPage from './Pages/PaymentPage';
 
 function App() {
   return (
     <div className="App">
-    
+      <CartPage/>
+      {/* <PaymentPage/> */}
     </div>
   );
 }
