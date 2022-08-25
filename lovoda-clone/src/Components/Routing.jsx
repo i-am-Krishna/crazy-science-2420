051971c6ce8714581cf2759ea951e62d01ca3e35
alok124-droid <alokkumardasd2016@gmail.com>
2022-08-25 22:24:20 +0530

    day2 navbar and all pages data

--- lovoda-clone/src/Components/Routing.jsx
+++ lovoda-clone/src/Components/Routing.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import AllPages from './AllPages'
+import Navbar from './Navbar'
 
 const Routing = () => {
   return (
     <div>
-        //Routing page start
+        
         <Routes>
+          <Route path='/' element={<Navbar/>}/>
             <Route exact path='/allpages' element={<AllPages/>}/>
         </Routes>
     </div>
