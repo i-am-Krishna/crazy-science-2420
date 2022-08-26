2cc435f334694e46ba3a355b37caff38d613ff01
alok124-droid <alokkumardasd2016@gmail.com>
2022-08-26 21:17:34 +0530

    payment request page

--- lovoda-clone/src/App.js
+++ lovoda-clone/src/App.js
@@ -1,12 +1,14 @@
 
 import MainRoutes from './Routes/MainRoutes';
 import Navbar from './Components/Navbar'
+// import PaymentSuccess from './Pages/PaymentSuccess';
 
 function App() {
   return (
     <div>
       <Navbar/>
      <MainRoutes/>
+     {/* <PaymentSuccess/> */}
     </div>
   );
 }
