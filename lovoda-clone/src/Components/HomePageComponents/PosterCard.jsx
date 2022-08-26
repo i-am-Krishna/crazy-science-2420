8f55182ac77a4c458909c98db371d2c6a30788e0
Rushikesh7997 <pardhikar26@gmail.com>
2022-08-26 22:05:01 +0530

    day4 update

--- lovoda-clone/src/Components/HomePageComponents/PosterCard.jsx
+++ lovoda-clone/src/Components/HomePageComponents/PosterCard.jsx
@@ -7,7 +7,7 @@ export const PosterCard = () => {
         <img src="https://cdn.shopify.com/s/files/1/0627/7388/7215/files/13122-2_1500x.jpg?v=1645120932" alt="" className='bigimage' />
         <div className='centered'> 
             <h2>NEW NEW NEW</h2>
-            <p>Check out the new beauties</p>
+            <p>Check out the new beauties</p><br /><br />
             <div className='shopAll_button'>Shop All</div>
         </div>
     </div>
