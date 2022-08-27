aa53f9b8010a26e4575e8673da4bdd0477961f78
Rushikesh7997 <pardhikar26@gmail.com>
2022-08-27 13:27:16 +0530

    day 5 up2

--- lovoda-clone/src/Components/HomePageComponents/PosterCard.jsx
+++ lovoda-clone/src/Components/HomePageComponents/PosterCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./posterCard.css"
+import { Link } from 'react-router-dom'
 
 export const PosterCard = () => {
   return (
@@ -7,8 +8,10 @@ export const PosterCard = () => {
         <img src="https://cdn.shopify.com/s/files/1/0627/7388/7215/files/13122-2_1500x.jpg?v=1645120932" alt="" className='bigimage' />
         <div className='centered'> 
             <h2>NEW NEW NEW</h2>
-            <p>Check out the new beauties</p>
+            <p>Check out the new beauties</p><br /><br />
+            <Link to={'/all_products'}>
             <div className='shopAll_button'>Shop All</div>
+            </Link>
         </div>
     </div>
   )
